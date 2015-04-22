dda6bc9dbf9eadc2cecfa9a305133a3e759ad351
Andreas Hallberg <anhallbe@gmail.com>
2015-04-22 16:20:50 +0200

    real piechart data

--- microFeedback/assets/js/javascript.js
+++ microFeedback/assets/js/javascript.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
 
 
-/*function pushBtn(data){
+function pushBtn(data){
 
     console.log("lol");
 }
 
-$('#great').click(pushBtn);
+$('#great').click(function(data) {
+  console.log("Great clicked!");
+    $.post("/press", {
+      name:'asd',
+      description:'some description',
+      option:'awesome'
+    });
+});
 
+$('#meh').click(function(data) {
+  console.log("Meh clicked!");
+  $.post('/press',{
+    name:'asd',
+    description:'some description',
+    option:'meh'
+    });
+});
+
+$('#terrible').click(function(data) {
+  console.log("Terrible clicked!");
+  $.post('/press',{
+    name:'asd',
+    description:'some description',
+    option:'terrible'
+    });
+});
 
 
 
@@ -20,34 +44,79 @@ function save(opinion){
     }
     });
 
-}*/
+}
 
 var ctx = document.getElementById("myChart").getContext("2d");
 var cty = document.getElementById("myOtherChart").getContext("2d");
 
+var nAwesome = 0;
+var nMeh = 0;
+var nTerrible = 0;
+
+$.get('/press/aggregate', function(ad) {
+  console.log(ad);
+
+  for(var i=0; i<ad.length; i++) {
+    console.log(ad[i]);
+
+    for(var o=0; o<ad[i].options.length; o++) {
+      if(ad[i].options[o].option == "awesome")
+        nAwesome += ad[i].options[o].count;
+      if(ad[i].options[o].option == "meh")
+        nMeh += ad[i].options[o].count;
+      if(ad[i].options[o].option == "terrible")
+        nTerrible += ad[i].options[o].count;
+    }
+  }
+
+  console.log("Awesome: " + nAwesome + " Meh: " + nMeh + " Terrible " + nTerrible);
+
+  var data = [
+      {
+          value: nTerrible,
+          color:"#F7464A",
+          highlight: "#FF5A5E",
+          label: "Terrible"
+      },
+      {
+          value: nAwesome,
+          color: "#46BFBD",
+          highlight: "#5AD3D1",
+          label: "Great!"
+      },
+      {
+          value: nMeh,
+          color: "#FDB45C",
+          highlight: "#FFC870",
+          label: "Meh"
+      }
+  ]
+  var myDoughnutChart = new Chart(ctx).Doughnut(data,Chart.defaults.global);
+});
+
 console.log(ctx);
-var data = [
+/*var data = [
     {
-        value: 50,
+        value: nTerrible,
         color:"#F7464A",
         highlight: "#FF5A5E",
         label: "Terrible"
     },
     {
-        value: 500,
+        value: nAwesome,
         color: "#46BFBD",
         highlight: "#5AD3D1",
         label: "Great!"
     },
     {
-        value: 100,
+        value: nMeh,
         color: "#FDB45C",
         highlight: "#FFC870",
         label: "Meh"
     }
 ]
 var myDoughnutChart = new Chart(ctx).Doughnut(data,Chart.defaults.global);
-
+*/
 
 
 var data = {
