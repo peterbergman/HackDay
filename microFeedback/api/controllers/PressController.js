0876cb7bad1fda7e595352283b1fd4d87b161ffc
Peter Bergman <peter.bergman@live.se>
2015-04-22 13:45:56 +0200

    Updated controller

--- microFeedback/api/controllers/PressController.js
+++ microFeedback/api/controllers/PressController.js
@@ -1,12 +1,41 @@
 /**
- * PressController
- *
- * @description :: Server-side logic for managing presses
- * @help        :: See http://links.sailsjs.org/docs/controllers
- */
+* PressController
+*
+* @description :: Server-side logic for managing presses
+* @help        :: See http://links.sailsjs.org/docs/controllers
+*/
 
 module.exports = {
-  lastDay: function(req, res) {
-    res.send('Hi there');
-  }
-};
+	lastDay: function(req, res) {
+		Press.native(function(errror, collection){
+			collection.aggregate(
+				{
+					$project:
+					{
+						'date': {'$substr': ['$createdAt', 0, 13] },
+						'option': '$option'
+					},
+				},
+				{
+					$group: {
+						'_id': '$date',
+						total: {
+							$sum: 1
+						},
+						options: {
+							'$push': {'option': '$option'}
+						}
+					},
+				},
+				{
+					$sort: {
+						'_id' : 1
+					}
+				}, function(e, r) {
+						console.log(r);
+						res.send(r);
+						if (e) return res.serverError(e);
+					});
+				})
+			}
+		};
