ab0e955490a55d7009957d59601d6dd492ef8a11
Peter Bergman <peter.bergman@live.se>
2015-04-22 09:35:45 +0200

    Addeed press api

---
+++ microFeedback/api/controllers/PressController.js
@@ -0,0 +1,11 @@
+/**
+ * PressController
+ *
+ * @description :: Server-side logic for managing presses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
